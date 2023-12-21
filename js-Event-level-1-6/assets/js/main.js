1a4a8e0dd914eea3c7ba0595409c7014dddd13c3
webkopp <nakopps@googlemail.com>
2023-12-21 17:05:32 +0100

    event-2-1

--- js-Event-level-1-6/assets/js/main.js
+++ js-Event-level-1-6/assets/js/main.js
@@ -2,3 +2,4 @@
 // - Der Eventhandler change wird bei einer erfolgten Änderung ausgelöst. Diese Wertänderung wird im Normalfall in einem Formularelement stattfinden.
 // - Schreibe eine Funktion, die im Paragraph den Text anpasst, nachdem du ein Element aus der Liste ausgewählt hast.
 // - Nutze: event.target.value, addEventListener("change", (event) => {...} ) und innerHTML oder textContent.
+
--- js-Event-level-2-1/assets/js/main.js
+++ js-Event-level-2-1/assets/js/main.js
@@ -1,2 +1,7 @@
 // - Schreibe eine kleine App, die die Hintergrundfarbe deiner Webseite ändert. Schaue dir die Vorschau an und versuche es umzusetzen.
 // - HTML und CSS befinden sich im Code-Snippet.
+
+function Mycolor() {
+    let element = document.getElementById("#grauTaste")
+    element.style.backgroundColor = "plum"
+    }
